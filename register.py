583d31177863208d436618dcbb528650d7bd64fb
ericamalia <ericamaliafong@gmail.com>
2016-07-27 10:05:05 -0700

    Delete register.py

--- register.py
+++
@@ -1,9 +0,0 @@
-import io
-
-reg_user = input('Please type in your username: ')
-reg_pass = input('Please type in your password: ')
-info = reg_user + "," + reg_pass + "\n"
-
-with open("users.txt", "a") as myfile:
-    myfile.write(info)
-    
